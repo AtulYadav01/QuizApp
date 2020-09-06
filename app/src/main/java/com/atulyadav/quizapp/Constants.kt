ce542f02eeed8c48fa672a8e80adbbc6e3b35814
Ashu1533 <ashuy033@gmail.com>
2020-09-06 09:24:51 +0530

    Added New Functionality

--- app/src/main/java/com/atulyadav/quizapp/Constants.kt
+++ app/src/main/java/com/atulyadav/quizapp/Constants.kt
@@ -5,8 +5,125 @@ object Constants {
         val questionsList = ArrayList<Question>()
 
         val que1 = Question(
-            1, "What country does this flag belong to?",
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_bahamas,
+            "Bahamas",
+            "Australia",
+            "Armenia",
+            "Austria",
+            1
+        )
+        questionsList.add(que1)
 
-            )
+        val que2 = Question(
+            2,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_brazil,
+            "Israel",
+            "Brazil",
+            "Norway",
+            "Sweden",
+            2
+        )
+        questionsList.add(que2)
+
+        val que3 = Question(
+            3,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_australia,
+            "Australia",
+            "New Zealand",
+            "United Kingdom",
+            "USA",
+            1
+        )
+        questionsList.add(que3)
+
+
+        val que4 = Question(
+            4,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_canada,
+            "Bhutan",
+            "Pakistan",
+            "Albenia",
+            "Canada",
+            4
+        )
+        questionsList.add(que4)
+
+        val que5 = Question(
+            5,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_germany,
+            "Germany",
+            "China",
+            "Russia",
+            "Mexico",
+            1
+        )
+        questionsList.add(que5)
+
+        val que6 = Question(
+            6,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_india,
+            "Panama",
+            "India",
+            "Chile",
+            "Bolivia",
+            2
+        )
+        questionsList.add(que6)
+
+        val que7 = Question(
+            7,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_israel,
+            "Cuba",
+            "Bangladesh",
+            "Israel",
+            "Egypt",
+            3
+        )
+        questionsList.add(que7)
+
+        val que8 = Question(
+            8,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_newzealand,
+            "Bolivia",
+            "India",
+            "Australia",
+            "New Zealand",
+            4
+        )
+        questionsList.add(que8)
+
+        val que9 = Question(
+            9,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_southafrica,
+            "Denmark",
+            "Chile",
+            "Bahamas",
+            "South Africa",
+            4
+        )
+        questionsList.add(que9)
+
+        val que10 = Question(
+            10,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_sweden,
+            "Nigeria",
+            "Sweden",
+            "Mexico",
+            "Madagascar",
+            2
+        )
+        questionsList.add(que10)
+        return questionsList
     }
 }
--- app/src/main/java/com/atulyadav/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/atulyadav/quizapp/QuizQuestionActivity.kt
@@ -1,11 +1,15 @@
 package com.atulyadav.quizapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class QuizQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
+
+        val questionsList = Constants.getQuestions()
+        Log.i("Questions Size", "${questionsList.size}")
     }
 }
